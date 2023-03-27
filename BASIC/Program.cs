9bb74f940ff7861aac0df1ad8f99c6bf6596983a
alican yılmaz <alicanyilmaz101@gmail.com>
2023-03-27 22:32:58 +0300

    Interface used for variable definition.

--- BASIC/Program.cs
+++ BASIC/Program.cs
@@ -5,15 +5,15 @@ using BASIC.Managers.CardServiceManager.WebServiceManager;
 
 // A.) Calling WebService
 var webServiceRequest = new WebService.Entities.Common.CardValidationRequest();
-var webServiceManager = new WebServiceManager();
+IWebServiceManager webServiceManager = new WebServiceManager();
 var webServiceResponse = CardServiceManager.Instance.ValidateDebitCardRequest(webServiceRequest, webServiceManager);
 
 // B.) Esb Call
 var esbServiceRequest = new EsbService.Entities.Common.CardValidationRequest();
-var esbServiceManager = new EsbServiceManager();
+IEsbServiceManager esbServiceManager = new EsbServiceManager();
 var esbServiceResponse = CardServiceManager.Instance.ValidateDebitCardRequest(esbServiceRequest, esbServiceManager);
 
 // C.) OnlyEsb Call
 var onlyServiceRequest = new EsbService.Entities.NonCommon.DoCashWithDrawalRequest();
-var onlyServiceServiceManager = new EsbServiceManager();
+IEsbServiceManager onlyServiceServiceManager = new EsbServiceManager();
 var onlyEsbServiceResponse = onlyServiceServiceManager.DoCash(onlyServiceRequest);
