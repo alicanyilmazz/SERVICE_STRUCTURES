32450466ef83a02845b3f431e9b28e2279ee7ab0
alican yılmaz <alicanyilmaz101@gmail.com>
2023-04-05 01:22:56 +0300

    Logger and Response added.

--- ORM/DalOperations/MySqlCustomerDal.cs
+++ ORM/DalOperations/MySqlCustomerDal.cs
@@ -1,5 +1,7 @@
-﻿using ORM.Entities;
+﻿using Microsoft.Extensions.DependencyInjection;
+using ORM.Entities;
 using ORM.IDalOperations;
+using ORM.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,19 +12,59 @@ namespace ORM.DalOperations
 {
     public class MySqlCustomerDal : ICustomerDal
     {
+        private static readonly LoggerHandler _provider =  NLogConfig._serviceProvider.GetRequiredService<LoggerHandler>();
         public Response Add(Customer Entity)
         {
-            Console.WriteLine($"MySql : {Entity.Name} added.");
+            try
+            {
+                Console.WriteLine($"MySql : {Entity.Name} added.");
+            }
+            catch (Exception ex)
+            {
+                _provider.LogError(ex.Message, ex);
+                return Response.Fail(ex.Message, 500, false);
+            }
+            finally
+            {
+                _provider.LogInfo("Success");
+            }
+            return Response.Success(Entity, 200);
         }
 
         public Response Delete(Customer Entity)
         {
-            Console.WriteLine($"MySql : {Entity.Name} deleted.");
+            try
+            {
+                Console.WriteLine($"MySql : {Entity.Name} deleted.");
+            }
+            catch (Exception ex)
+            {
+                _provider.LogError(ex.Message, ex);
+                return Response.Fail(ex.Message, 500, false);
+            }
+            finally
+            {
+                _provider.LogInfo("Success");
+            }
+            return Response.Success(Entity, 200);
         }
 
         public Response Update(Customer Entity)
         {
-            Console.WriteLine($"MySql : {Entity.Name} updated.");
+            try
+            {
+                Console.WriteLine($"MySql : {Entity.Name} updated.");
+            }
+            catch (Exception ex)
+            {
+                _provider.LogError(ex.Message, ex);
+                return Response.Fail(ex.Message, 500, false);
+            }
+            finally
+            {
+                _provider.LogInfo("Success");
+            }
+            return Response.Success(Entity, 200);
         }
     }
 }
