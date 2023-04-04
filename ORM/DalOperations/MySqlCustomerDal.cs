4d13dc2b14a81059f01c39c138c9daace4c55784
alican yılmaz <alicanyilmaz101@gmail.com>
2023-04-04 23:31:18 +0300

    MySqlCustomerDal created.

---
+++ ORM/DalOperations/MySqlCustomerDal.cs
@@ -0,0 +1,28 @@
+﻿using ORM.Entities;
+using ORM.IDalOperations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM.DalOperations
+{
+    public class MySqlCustomerDal : ICustomerDal
+    {
+        public Response Add(Customer Entity)
+        {
+            Console.WriteLine($"MySql : {Entity.Name} added.");
+        }
+
+        public Response Delete(Customer Entity)
+        {
+            Console.WriteLine($"MySql : {Entity.Name} deleted.");
+        }
+
+        public Response Update(Customer Entity)
+        {
+            Console.WriteLine($"MySql : {Entity.Name} updated.");
+        }
+    }
+}
