4ccb87d8cb31fafc3b7b1458cd254741f000d7d5
alican yılmaz <alicanyilmaz101@gmail.com>
2023-04-05 01:23:18 +0300

    LoggerHandler created.

---
+++ ORM/Utilities/LoggerHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM.Utilities
+{
+    internal class LoggerHandler
+    {
+        private readonly ILogger _logger;
+        public LoggerHandler(ILogger<LoggerHandler> logger)
+        {
+            _logger = logger;
+        }
+        public void LogError(string message,Exception exception)
+        {
+            _logger.LogError(exception,message);
+        }
+
+        public void LogInfo(string message)
+        {
+            _logger.LogInformation(message);
+        }
+    }
+}
